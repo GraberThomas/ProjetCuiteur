539563863bef76352b0363829cef5f1da84bf62c
Logan-Developer <logan2.humbert@gmail.com>
2022-05-16 19:20:09 +0200

    Fix delete blabla

--- php/supprimer_blabla.php
+++ php/supprimer_blabla.php
@@ -17,11 +17,13 @@ $request = 'SELECT * FROM blablas WHERE blID ='.$_GET['idBlabla'];
 $request = gh_bd_proteger_entree($db, $request);
 $result = gh_bd_send_request($db, $request);
 if(mysqli_num_rows($result) == 0){
+    mysqli_free_result($result);
+    mysqli_close($db);
     header('Location: ../index.php');
     exit;
 }
 $row = mysqli_fetch_assoc($result);
-if($row['blAuteur'] != $_SESSION['usID']){
+if($row['blIDAuteur'] != $_SESSION['usID']){
     header('Location: ../index.php');
     exit;
 }
@@ -30,17 +32,14 @@ $request = 'DELETE FROM mentions WHERE meIDBlabla ='.$_GET['idBlabla'];
 $request = gh_bd_proteger_entree($db, $request);
 $result = gh_bd_send_request($db, $request);
 
-mysqli_free_result($result);
-$request = 'DELETE FROM tags WHERE taIDBlablas ='.$_GET['idBlabla'];
+$request = 'DELETE FROM tags WHERE taIDBlabla ='.$_GET['idBlabla'];
 $request = gh_bd_proteger_entree($db, $request);
 $result = gh_bd_send_request($db, $request);
 
-mysqli_free_result($result);
 $request='DELETE FROM blablas WHERE blID ='.$_GET['idBlabla'];
 $request = gh_bd_proteger_entree($db, $request);
 gh_bd_send_request($db, $request);
 
-mysqli_free_result($result);
 mysqli_close($db);
 
 header('Location: ../index.php');

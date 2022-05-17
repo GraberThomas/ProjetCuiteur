394aa65fd2968003b1109e87c73abc5b4f101fb2
Logan-Developer <logan2.humbert@gmail.com>
2022-05-17 18:04:20 +0200

    recherche: Don't allow searching if field is empty

--- php/recherche.php
+++ php/recherche.php
@@ -16,14 +16,20 @@ if (! gh_est_authentifie()){
 - Do search if form is submitted
 ------------------------------------------------------------------------------*/
 $row;
-if(isset($_POST['recherche'])){
-    $db = gh_bd_connect();
-    $recherche = gh_bd_proteger_entree($db, $_POST['recherche']);
-    $request = "SELECT DISTINCT users.*
-                FROM users
-                WHERE (usPseudo LIKE '%$recherche%' OR usNom LIKE '%$recherche%')";
-                
-    $result = gh_bd_send_request($db, $request);
+if(isset($_POST['btnRechercher'])){
+    if( !gh_parametres_controle('post', array('recherche', 'btnRechercher'))) {
+        gh_session_exit();   
+    }
+
+    if ($_POST['recherche'] != '') {
+        $db = gh_bd_connect();
+        $recherche = gh_bd_proteger_entree($db, $_POST['recherche']);
+        $request = "SELECT DISTINCT users.*
+                    FROM users
+                    WHERE (usPseudo LIKE '%$recherche%' OR usNom LIKE '%$recherche%')";
+                    
+        $result = gh_bd_send_request($db, $request);
+    }
 }
 
 
@@ -37,13 +43,17 @@ gh_aff_debut('Cuiteur | Recherche', '../styles/cuiteur.css');
 gh_aff_entete('Rechercher des utilisateurs', true);
 gh_aff_infos(true);
 
+if (isset($_POST['btnRechercher']) && $_POST['recherche'] == '') {
+    echo '<p class="error">Veuillez entrer un terme de recherche</p>';
+}
+
 echo '<form  id="recherche" action="recherche.php" method="post">',
         '<table>',
-            '<tr><td><input type=text name="recherche" size=30 maxlength=30></td>',
-                '<td><input type=submit value="Rechercher"></td></tr>',
+            '<tr><td><input type=text name="recherche" size=30 maxlength=30 required></td>',
+                '<td><input type=submit name="btnRechercher" value="Rechercher"></td></tr>',
         '</table>',
     '</form>';
-if(isset($_POST['recherche'])){
+if(isset($_POST['recherche']) && $_POST['recherche'] != ''){
     gh_aff_user_stats_list($result, $db);
     // free resources
     mysqli_free_result($result);

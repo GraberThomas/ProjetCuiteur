2cb89dd4d28b104ee674530423f8ac8840a573cf
Logan-Developer <logan2.humbert@gmail.com>
2022-05-18 11:03:54 +0200

    utilisateur: Add subscribe/unsubscribe button

--- php/utilisateur.php
+++ php/utilisateur.php
@@ -43,7 +43,7 @@ gh_aff_entete('Le profil de '. $userStats['usPseudo']);
 gh_aff_infos(true);
 
 gh_aff_user_stats($userStats);
-gh_aff_user_info($userData);
+gh_aff_user_info($db, $userData);
 
 gh_aff_pied();
 gh_aff_fin();
@@ -59,9 +59,11 @@ mysqli_close($db);
     /**
      * Show user's info
      *
+     * @param mysqli $db database connection
      * @param array $userData User's data
      */
-    function gh_aff_user_info(array $userData){
+    function gh_aff_user_info(mysqli $db, array $userData){
+
         echo '<article id="userInfo">',
                 '<table>',
                     '<tr>',
@@ -73,17 +75,51 @@ mysqli_close($db);
                         '<td>'. gh_amj_clair($userData['usDateInscription']) .'</td>',
                     '</tr>',
                     '<tr>',
-                        '<td>Vile de résidence :</td>',
-                        '<td>'. $userData['usVille'] .'</td>',
+                        '<td>Ville de résidence :</td>',
+                        '<td>';
+                            if (empty($userData['usVille'])){
+                                echo 'Non renseignée';
+                            } else {
+                                echo $userData['usVille'];
+                            }
+                    echo'</td>',
                     '</tr>',
                     '<tr>',
                         '<td>Mini-bio :</td>',
-                        '<td>'. $userData['usBio'] .'</td>',
+                        '<td>';
+                            if (empty($userData['usBio'])){
+                                echo 'Non renseignée';
+                            } else {
+                                echo $userData['usBio'];
+                            }
+                    echo'</td>',
                     '</tr>',
                     '<tr>',
                         '<td>Site web :</td>',
-                        '<td>'. $userData['usWeb'] .'</td>',
-                    '</tr>',
-                '</table>',
+                        '<td>';
+                            if (empty($userData['usSiteWeb'])){
+                                echo 'Non renseigné';
+                            } else {
+                                echo '<a href="'. $userData['usSiteWeb'] .'" target="_blank">'. $userData['usSiteWeb'] .'</a>';
+                            }
+                    echo'</td>',
+                    '</tr>';
+
+                if ($userData['usID'] != $_SESSION['usID']){
+                    echo '<tr>',
+                            '<td>',
+                                '<form action="./sabonner.php" method="post">';
+                                    if (gh_sql_check_subscription($db, $_SESSION['usID'], $userData['usID'])){
+                                        echo '<input type="submit" name="btnValiderAbonnement" value="Se désabonner" />',
+                                            '<input type="hidden" name="desabonnement_'. $userData['usID'] .'" value="'. $userData['usID'] .'" />';
+                                    } else {
+                                        echo '<input type="submit" name="btnValiderAbonnement" value="S\'abonner" />',
+                                            '<input type="hidden" name="abonnement_'. $userData['usID'] .'" value="'. $userData['usID'] .'" />';
+                                    }
+                    echo        '</form>',
+                            '</td>',
+                        '</tr>';
+                }
+        echo    '</table>',
             '</article>';
     }
